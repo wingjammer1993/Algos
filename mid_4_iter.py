8f0d04e67db2b687a55306de158bfc1755cfc8d5
Amruta <hail.radical@gmail.com>
2017-11-04 15:59:55 -0600

    improved iteration

--- mid_4_iter.py
+++ mid_4_iter.py
@@ -3,30 +3,35 @@ import math
 
 
 def min_penalty(a, p, q):
-    p_matrix = numpy.empty((len(a), len(a)))
-    p_matrix[:] = numpy.float('inf')
+    p_matrix = [float('inf')]*len(a)
+    back_track = {}
 
     for i in range(q, p-1, -1):
         for j in range(i, q+1):
             num = sum(a[i:j + 1])
             if num <= 200:
                 if j == q:
-                    p_matrix[i][j] = charge(num)
+                    val = charge(num)
+                    if val < p_matrix[i]:
+                        p_matrix[i] = val
+                        back_track[i] = i, j
                 elif i <= j:
                     num = charge(num)
-                    num_min = numpy.min(p_matrix[j+1])
-                    p_matrix[i][j] = num + num_min
+                    num_min = p_matrix[j+1]
+                    val = num + num_min
+                    if val < p_matrix[i]:
+                        p_matrix[i] = val
+                        back_track[i] = i, j
 
     tour_path = []
     k = 0
-    i = 0
+    print(back_track)
     while k < len(p_matrix):
-        j = numpy.argmin(p_matrix[k])
-        tour_path.append((i, j))
-        i = j+1
+        m, j = back_track[k]
+        tour_path.append((m, j))
         k = j+1
 
-    return numpy.min(p_matrix[0]), tour_path
+    return p_matrix[0], tour_path
 
 
 def charge(n):
@@ -34,6 +39,7 @@ def charge(n):
 
 
 if __name__ == "__main__":
-    arr = [130, 50, 150, 50, 80, 200, 120, 40, 40]
+    arr = [130, 50, 150, 50, 50, 50, 50, 150]
     prob = min_penalty(arr, 0, len(arr)-1)
     print(prob)
+
